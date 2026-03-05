a2355cb77464bb61d44ae47827b20f719d654760
Julian Kaffke <julian@42nerds.com>
2026-03-05 16:25:03 +0100

    fix(cli): populate version from Go build info for go install
    Fall back to runtime/debug.ReadBuildInfo() so that `go install @latest`
    shows the module version and VCS metadata instead of "dev / unknown".

--- cmd/ptouch/version.go
+++ cmd/ptouch/version.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -12,14 +13,30 @@ var (
 	date    = "unknown"
 )
 
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok && version == "dev" {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				if len(s.Value) >= 7 {
+					commit = s.Value[:7]
+				}
+			case "vcs.time":
+				date = s.Value
+			}
+		}
+	}
+
+	rootCmd.AddCommand(versionCmd)
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version of ptouch",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("ptouch %s (commit %s, built %s)\n", version, commit, date)
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(versionCmd)
 }
